6e4d0bf64aa5b51484007a33c9c08c7aae81f811
Tranchant2001 <martin.guillon.perso@gmail.com>
2023-10-12 19:06:57 +0200

    New file .py and output folder for 2nd part of the project n°2.
    Tests with the Jacobi method and outputs produced.

--- Project2/3_solution_poisson.py
+++ Project2/3_solution_poisson.py
@@ -12,7 +12,7 @@ Mis à jour le 11/10/2023.
 v1.1
 
 DESCRIPTION:
-Version answering to the laplacian operator discussion.
+Version answering to the Poisson equation.
 
 """
 ### PACKAGES    ###
@@ -53,6 +53,11 @@ def analy_laplacian(u):
     return -2*(piL_p2)*u
 
 
+def update_jacobi(vecu, b_vec):
+
+    return -0.25*(-np.roll(vecu, -N) - np.roll(vecu, -1) - np.roll(vecu, 1) - np.roll(vecu, N) + b_vec)
+
+
 def error_estimation_laplacian(u, ux4_plus_uy4, A2u, a_A2u):
 
     rel_diff = (a_A2u - A2u).sum() / (N**2)
@@ -75,13 +80,17 @@ def plot_scalar_field(some_array, **kwargs):
     ax.clear()
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
+
     if 'title' in kwargs.keys():
         ax.set_title(kwargs.get('title'))
     ax.imshow(some_array, extent=(0, L, 0, L), origin='lower', cmap='viridis')
 
     if 'saveaspng' in kwargs.keys():
-        plt.savefig(dirpath+"/results_2-laplacian-operator/"+kwargs.get('saveaspng'), dpi=128, bbox_inches="tight")
-    plt.pause(5)
+        plt.savefig(dirpath+"/results_3-solution-poisson/"+kwargs.get('saveaspng'), dpi=128, bbox_inches="tight")
+    
+    if 'pause' in kwargs.keys():
+        plt.pause(kwargs.get("pause"))
+
     plt.close(fig)
 
 
@@ -97,18 +106,17 @@ L = 1.0     # Length of the (square shaped) domain (m)
 
 # Initial Parameters of the simulation
 #N_list = np.array([4,5,6,7,8,12,16,24,32,64,128,256,512]) # Number of steps for each space axis
-N_list = np.array([64])
+N_list = np.array([20])
 len_Nl = N_list.shape[0]
 
 # Useful constants
-piL_p2 = (np.pi/L)**2
+dpdz = 1000
+mu = 1e-3
+beta = dpdz/mu
+piL_p2 = (np.pi/L)**2 #pi over L squared
 
-# Put here the maximum time you want to spend on the computation.
-max_time_computation = dt.timedelta(hours=6)
-
-error_list = np.zeros(len_Nl, dtype=float)
-leading_error_list = np.zeros(len_Nl, dtype=float)
-num_lead_list = np.zeros(len_Nl, dtype=float)
+#Jacobi number of iterations
+n_jacobi = 10
 
 
 for k in range(len_Nl):
@@ -127,40 +135,26 @@ for k in range(len_Nl):
 
     # Initial Uz field
     U = get_Uz_field(X, Y)
+    Ushape = U.shape
 
-    # 2nd derivatives
-    U_xx = snd_x_deriv(U)
-    U_yy = snd_y_deriv(U)
+    # vecteur linearized
+    vecU = U.flatten()
+    len_vecU = vecU.shape[0]
+    vec_beta = np.full(len_vecU, beta)
 
-    # Laplacian estimated
-    A2U = U_xx + U_yy
-
-    plot_scalar_field(A2U, title="Laplacian before crop")
-
-    # sum of 4th derivatives
-    U_x4_plus_U_y4 = laplacian_operator(A2U) - 2*(snd_x_deriv(U_yy))
-
-
-    # Analytical Laplacian
-    analy_A2U = analy_laplacian(U)
-
-    # I crop the borders because they are source of peak instability.
+    # Plot of the initial U field. I crop the borders because they are source of peak instability.
     U_crop = np.delete(np.delete(U, [0], 0), [0], 1)
-    A2U_crop = np.delete(np.delete(A2U, [0], 0), [0], 1)
-    a_A2U_crop = np.delete(np.delete(analy_A2U, [0], 0), [0], 1)
-    U_x4_sum_crop = np.delete(np.delete(U_x4_plus_U_y4, [0], 0), [0], 1)
+    plot_scalar_field(U_crop, title="$U_{0}$", saveaspng="N"+str(N)+"_Uindex-0.png", pause=3)
+    
+    for k in range(1,n_jacobi+1):
+        
+        vecU = update_jacobi(vecU, vec_beta)
+        U_crop = np.delete(np.delete(np.reshape(vecU, Ushape), [0], 0), [0], 1)
+        plot_scalar_field(U_crop, title="$U_{"+str(k)+"}$", saveaspng="N"+str(N)+"_Uindex-"+str(k)+".png", pause=3)
 
     #plot_scalar_field(U_crop, title="U field (N="+str(N)+")", saveaspng="N"+str(N)+"_U_field.png")
-    plot_scalar_field(A2U_crop, title="Estimated LU field (N="+str(N)+")", saveaspng="N"+str(N)+"_estLU_field.png")
-    #plot_scalar_field(a_A2U_crop, title="Analytic LU field (N="+str(N)+")", saveaspng="N"+str(N)+"_analyLU_field.png")
-    #plot_scalar_field(a_A2U_crop-A2U_crop, title="Diffference error (N="+str(N)+")", saveaspng="N"+str(N)+"_diff-error_field.png")
 
-    diff_error, leading_error, num_lead_error = error_estimation_laplacian(U_crop, U_x4_sum_crop, A2U_crop, a_A2U_crop)
-
-    error_list[k] = abs(diff_error)
-    leading_error_list[k] = abs(leading_error)
-    num_lead_list[k] = abs(num_lead_error)
-
+"""
 dx_list = L/N_list
 plt.loglog(dx_list, error_list, label="Difference $|\Delta u - \Delta u^{num}|\slash N^2$")
 plt.loglog(dx_list, leading_error_list, label="Analytical $sum(\epsilon_{ij})\slash N^2$")
@@ -170,32 +164,4 @@ plt.ylabel("Error (a.u.)")
 plt.legend()
 plt.savefig(dirpath+"/results_2-laplacian-operator/erros_logf-of-logdx.png", dpi=128, bbox_inches="tight")
 plt.show()
-
-
-plt.loglog(N_list, error_list, label="Difference $|\Delta u - \Delta u^{num}|\slash N^2$")
-plt.loglog(N_list, leading_error_list, label="Analytical $sum(\epsilon_{ij})\slash N^2$")
-plt.loglog(N_list, num_lead_list, label="Numerical $sum(\epsilon_{ij}^{num})\slash N^2$")
-plt.xlabel("log N")
-plt.ylabel("log Error (a.u.)")
-plt.legend()
-plt.savefig(dirpath+"/results_2-laplacian-operator/erros_logf-of-logN.png", dpi=64, bbox_inches="tight")
-plt.show()
-
-plt.plot(dx_list, error_list, label="Difference $|\Delta u - \Delta u^{num}|\slash N^2$")
-plt.plot(dx_list, leading_error_list, label="Analytical $sum(\epsilon_{ij})\slash N^2$")
-plt.plot(dx_list, num_lead_list, label="Numerical $sum(\epsilon_{ij}^{num})\slash N^2$")
-plt.xlabel("$\Delta x$")
-plt.ylabel("Error (a.u.)")
-plt.legend()
-plt.savefig(dirpath+"/results_2-laplacian-operator/erros_f-of-dx.png", dpi=128, bbox_inches="tight")
-plt.show()
-
-
-plt.plot(N_list, error_list, label="Difference $|\Delta u - \Delta u^{num}|\slash N^2$")
-plt.plot(N_list, leading_error_list, label="Analytical $sum(\epsilon_{ij})\slash N^2$")
-plt.plot(N, num_lead_list, label="Numerical $sum(\epsilon_{ij}^{num})\slash N^2$")
-plt.xlabel("N")
-plt.ylabel("Error (a.u.)")
-plt.legend()
-plt.savefig(dirpath+"/results_2-laplacian-operator/erros_f-of-N.png", dpi=64, bbox_inches="tight")
-plt.show()
+"""
